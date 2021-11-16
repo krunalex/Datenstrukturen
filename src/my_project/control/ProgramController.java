e16e42c24732fe712e9a5f05bd5caedfe73f708d
cengfati <cengfati@kkg-do.eu>
2021-11-16 10:11:07 +0100

    Hoffentlich geht es ahhhh

---
+++ src/my_project/control/ProgramController.java
@@ -0,0 +1,95 @@
+package my_project.control;
+
+import KAGO_framework.control.ViewController;
+import KAGO_framework.model.abitur.datenstrukturen.Queue;
+import KAGO_framework.model.abitur.datenstrukturen.Stack;
+import my_project.model.QueueBall;
+import my_project.model.StackSquare;
+import my_project.view.InputReceiver;
+
+import java.awt.event.MouseEvent;
+//import java.util.Stack;
+
+/**
+ * Ein Objekt der Klasse ProgramController dient dazu das Programm zu steuern. Die updateProgram - Methode wird
+ * mit jeder Frame im laufenden Programm aufgerufen.
+ */
+public class ProgramController {
+
+    //Attribute
+
+
+    // Referenzen
+    private ViewController viewController;  // diese Referenz soll auf ein Objekt der Klasse viewController zeigen. Über dieses Objekt wird das Fenster gesteuert.
+    private Queue<QueueBall> ballQueue;
+    private QueueBall lastBallinQueue;
+    private Stack<StackSquare> squareStack;
+    private StackSquare squareBeforeInStack;
+
+    /**
+     * Konstruktor
+     * Dieser legt das Objekt der Klasse ProgramController an, das den Programmfluss steuert.
+     * Damit der ProgramController auf das Fenster zugreifen kann, benötigt er eine Referenz auf das Objekt
+     * der Klasse viewController. Diese wird als Parameter übergeben.
+     * @param viewController das viewController-Objekt des Programms
+     */
+    public ProgramController(ViewController viewController){
+        this.viewController = viewController;
+    }
+
+    /**
+     * Diese Methode wird genau ein mal nach Programmstart aufgerufen.
+     * Sie erstellt die leeren Datenstrukturen, zu Beginn nur eine Queue
+     */
+    public void startProgram() {
+        // Für Benutzerinteraktion
+        new InputReceiver(this,viewController); // darf anonym sein, weil kein Zugriff nötig ist
+        // Für die Queue:
+        ballQueue = new Queue<>();
+        lastBallinQueue = null; // die letzte Kugel muss für die Animation gemerkt werden
+        squareStack = new Stack<>();
+        squareBeforeInStack = null;
+    }
+
+    public void addBallToQueue(){
+        QueueBall newQueueBall = new QueueBall(650,50,lastBallinQueue,viewController);
+        ballQueue.enqueue(newQueueBall);
+        lastBallinQueue = newQueueBall;
+    }
+
+    public void deleteBallFromQueue(){
+        if(!ballQueue.isEmpty()){
+            if(ballQueue.front().tryToDelete()) ballQueue.dequeue();
+        }
+    }
+
+    public void addSquare(){
+        StackSquare newStackSquare = new StackSquare(400,100, squareBeforeInStack, viewController);
+        squareStack.push(newStackSquare);
+            squareBeforeInStack = newStackSquare;
+
+    }
+
+    public void deleteSquare(){
+        if(!squareStack.isEmpty()){
+            if(squareStack.top().isDeletable()){
+                squareStack.pop();
+            }
+        }
+    }
+    /**
+     * Aufruf bei Mausklick
+     * @param e das Objekt enthält alle Informationen zum Klick
+     */
+    public void mouseClicked(MouseEvent e){
+
+    }
+
+    /**
+     * Aufruf mit jeder Frame
+     * @param dt Zeit seit letzter Frame
+     */
+    public void updateProgram(double dt){
+
+    }
+}
---
+++ src/my_project/model/StackSquare.java
@@ -0,0 +1,54 @@
+package my_project.model;
+
+import KAGO_framework.control.ViewController;
+import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.view.DrawTool;
+
+public class StackSquare extends GraphicalObject {
+
+    private ViewController viewController;
+    private StackSquare previousSquare;
+    protected boolean ready;
+    protected boolean removed;
+
+
+    public StackSquare(double x, double y,StackSquare previousSquare, ViewController viewController){
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        ready = false;
+        removed = false;
+        viewController.draw(this);
+        this.previousSquare = previousSquare;
+    }
+
+    public void draw(DrawTool drawTool){
+        drawTool.drawRectangle(x,y,50,30);
+    }
+
+    public void update(double dt) {
+        if(!ready){
+            if(previousSquare == null || y < previousSquare.getY()-40){
+                y += 100*dt;
+            }
+            if(y > 500){
+                ready = true;
+            }
+        }
+        if(removed){
+            x -= 100*dt;
+            /*if(x < -100){
+                viewController.removeDrawable(this);
+            } */
+        }
+    }
+
+    public boolean isDeletable(){
+        if(ready){
+            removed = true;
+            return true;
+        }
+        return false;
+    }
+}
